bd241febb28e9b4316db1aa7fdc07f5aff2582fd
Arthit LungYa <rthit096@gmail.com>
2023-10-09 04:56:03 +0700

    Latest

--- js/script.js
+++ js/script.js
@@ -59,6 +59,29 @@ function searchPage() {
     return false; // Prevent form submission
 }
 
+// ขึ้นไปบนสุดของเพจ
+// Get a reference to the button element
+const scrollToTopButton = document.getElementById('scrollToTopButton');
+
+// Add a click event listener to the button
+scrollToTopButton.addEventListener('click', () => {
+  // Scroll to the top of the page with a smooth animation
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth',
+  });
+});
+
+// Show or hide the button based on the user's scroll position
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 200) {
+    // Show the button when the user scrolls down 200 pixels or more
+    scrollToTopButton.style.display = 'block';
+  } else {
+    // Hide the button when the user is near the top of the page
+    scrollToTopButton.style.display = 'none';
+  }
+});
 
 
 
